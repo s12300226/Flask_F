26229a337065964699914a8294cc2d8a76779324
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-06-01 06:31:53 +0000

    layout.htmlを作成

--- tsubokawa/application/flask_blog/config.py
+++ tsubokawa/application/flask_blog/config.py
@@ -1,1 +1,3 @@
 DEBUG=True
+USERNAME='tsubo'
+PASSWORD='test'
--- tsubokawa/application/flask_blog/views.py
+++ tsubokawa/application/flask_blog/views.py
@@ -8,10 +8,31 @@ def xxx():
     処理
 
 """
-
+from flask import request, url_for, render_template, flash, session,redirect
 
 from flask_blog import app
 
 @app.route('/')
 def show_entries():
-    return "Hello The World!"
+    # templates/entries/index.htmlを返す設定
+    return render_template('entries/index.html')
+
+@app.route('/test')
+def show_tests():
+    # templates/entries/index.htmlを返す設定
+    return render_template('entries/test.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        if request.form['username']!=app.config['USERNAME']:
+            print('ユーザー名が違うよ')
+        elif request.form['password']!= app.config['PASSWORD']:
+            print('パスワードが違うよ')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    return redirect('/')
