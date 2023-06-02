c6515f699430e3af9c49d01faa4375eabe99e978
yamaji-youta <yamaji.youta.nd@mynavi.jp>
2023-06-02 08:18:41 +0000

    Add vlog by yamaji

--- yamaji/application/flask_blog/__init__.py
+++ yamaji/application/flask_blog/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app = Flask(__name__, static_folder='./templates/entries/imgs')
 
 app.config.from_object('flask_blog.config')
 
-import flask_blog.views
+db = SQLAlchemy(app)
+
+from flask_blog.views import views, entries
