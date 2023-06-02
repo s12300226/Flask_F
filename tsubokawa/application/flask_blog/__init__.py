41a4b76124aad096f7961ca90b1bb505a19e9870
yoichi-tsubo <tsubokawa.yoichi.ol@mynavi.jp>
2023-06-02 06:48:35 +0000

    DBテーブル作成

--- tsubokawa/application/flask_blog/__init__.py
+++ tsubokawa/application/flask_blog/__init__.py
@@ -1,12 +1,15 @@
 # Flaskインポート
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 # Flaskアプリケーション本体の作成
 app = Flask(__name__)
 
 # flask_blogフォルダ内,config.pyをconfigとして扱う
 app.config.from_object('flask_blog.config')
+
+
+db = SQLAlchemy(app)
 
 # viewsファイルのインポート
 import flask_blog.views
