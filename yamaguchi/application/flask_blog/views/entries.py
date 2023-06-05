bbdd7433ce30792bc66890693118605ab9c791d2
YukiYamaguchi43 <yamaguchi.yuki.ep@mynavi.jp>
2023-06-05 04:14:14 +0000

    lastone

--- yamaguchi/application/flask_blog/views/entries.py
+++ yamaguchi/application/flask_blog/views/entries.py
@@ -2,9 +2,11 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from flask_blog.models.entries import Entry
 from flask_blog import db
+from flask_blog import login_required
 
 
 @app.route('/')
+@login_required
 def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -12,12 +14,14 @@ def show_entries():
     return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('entries/new.html')
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -31,6 +35,7 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>', methods=['GET'])
+@login_required
 def show_entry(id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -38,6 +43,7 @@ def show_entry(id):
     return render_template('entries/show.html', entry=entry)
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
 def edit_entry(id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -45,6 +51,7 @@ def edit_entry(id):
     return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
     if not session.get('logged_in'):
         return redirect(url_for('login'))
@@ -54,4 +61,15 @@ def update_entry(id):
     db.session.merge(entry)
     db.session.commit()
     flash('記事が更新されました')
+    return redirect(url_for('show_entries'))
+
+@app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash('投稿が削除されました')
     return redirect(url_for('show_entries'))
