b2d3a99e8d901b0ee0f75594421fde1efbe0276c
YukiYamaguchi43 <yamaguchi.yuki.ep@mynavi.jp>
2023-06-05 00:46:18 +0000

    It's been a while

--- yamaguchi/application/flask_blog/config.py
+++ yamaguchi/application/flask_blog/config.py
@@ -2,3 +2,13 @@ DEBUG = True
 SECRET_KEY = 'secret key'
 USERNAME = 'Nissy'
 PASSWORD = 'Nissy930'
+
+import os
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8".format(**{
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", "mysql"),
+    "host":os.getenv("DB_HOST", "localhost"),
+    "database":os.getenv("DB_DATABASE", "ENSHU")
+})
+SQLALCHEMY_TRACK_MODIFICATION = False
