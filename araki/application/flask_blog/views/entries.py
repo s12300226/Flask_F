c123792cef8a60c4d65db16d926501b84a95a4c5
KoichiAraki <araki.koichi.ku@mynavi.jp>
2023-06-05 05:24:39 +0000

    CRUD

--- araki/application/flask_blog/views/entries.py
+++ araki/application/flask_blog/views/entries.py
@@ -28,3 +28,39 @@ def add_entry():
     db.session.commit()
     flash('新しく記事が作成されました')
     return redirect(url_for('show_entries'))
+
+@app.route('/entries/<int:id>',methods=['GET'])
+def show_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/show.html', entry=entry)
+
+@app.route('/entries/<int:id>/edit',methods=['GET'])
+def edit_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/edit.html', entry=entry)
+
+@app.route('/entries/<int:id>/update',methods=['POST'])
+def update_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    entry.title=request.form['title'],
+    entry.text=request.form['text']
+    db.session.merge(entry)
+    db.session.commit()
+    flash('記事が更新されました')
+    return redirect(url_for('show_entries'))
+
+@app.route('/entries/<int:id>/delete',methods=['POST'])
+def delete_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash('投稿が削除されました')
+    return redirect(url_for('show_entries'))
