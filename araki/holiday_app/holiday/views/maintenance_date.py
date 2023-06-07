72999c54f4b03d0511f84e9660ea062b2a46f6a4
KoichiAraki <araki.koichi.ku@mynavi.jp>
2023-06-07 05:38:38 +0000

    add co,

--- araki/holiday_app/holiday/views/maintenance_date.py
+++ araki/holiday_app/holiday/views/maintenance_date.py
@@ -5,28 +5,28 @@ from holiday import db
 
 
 @app.route('/maintenance_date', methods=['POST'])
-def change_holiday():
+def change_holiday():  #20文字以上の場合
     if len(request.form['holiday_text'])> 20:
         flash("文字数は20文字以内で入力してください")
         return redirect(url_for('show_entries'))
 
-    elif request.form['holiday'] == "":
+    elif request.form['holiday'] == "":  #日付が空欄の場合
         flash('日付を再度入力してください。')
         return redirect(url_for('show_entries'))
     
     elif request.form['button'] == 'insert_update':
-        if request.form['holiday_text'] == '':
-            flash('祝日テキストが空欄です。入力してください。')
+        if request.form['holiday_text'] == '': #holiday_textが空欄の場合
+            flash('祝日名を入力してください。')
             return redirect(url_for('show_entries'))
         holiday = Entry.query.get(request.form['holiday'])
 
-        if holiday != None:
+        if holiday != None:  #既にデータがある場合
             holiday.holi_text = request.form['holiday_text']        
             db.session.merge(holiday)
             db.session.commit()
             result_text = f'{holiday.holidate}は「{holiday.holi_text}」に更新されました'
         
-        else:
+        else:  #データがない場合
             holiday=Entry(
                 holidate = request.form['holiday'],
                 holi_text = request.form['holiday_text']
